a53fb3187a3c2e61fa6e6ae66fe8bb16906d6874
19essibusa <39638216+19essibusa@users.noreply.github.com>
2020-05-22 11:34:45 -0500

    CS112 Joel Adams, Victor Norman

---
+++ lab01/Application.h
@@ -0,0 +1,15 @@
+/* Application.h declares class Application
+ * Student Name: Einstein Essibu
+ * Date:02/17/2020
+ * Begun by: Joel Adams, for CS 112 at Calvin University.
+ */
+
+#ifndef APPLICATION_H_
+#define APPLICATION_H_
+
+class Application {
+public:
+	void RunApp();
+};
+
+#endif /* APPLICATION_H_ */
---
+++ lab01/Debug/main.d
@@ -0,0 +1,10 @@
+main.o: ../main.cpp ../SongTester.h ../Song.h ../PlayListTester.h \
+ ../Application.h
+
+../SongTester.h:
+
+../Song.h:
+
+../PlayListTester.h:
+
+../Application.h:
